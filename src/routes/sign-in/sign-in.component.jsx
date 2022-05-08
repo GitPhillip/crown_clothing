1f8189b9cf6368f167be897ab5a24dea70d61bc8
Phillip Ndlovu <216011438@student.uj.ac.za>
2022-05-08 17:55:47 +0200

    Firebase redirect sign in's with auth state persisting

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,44 @@
 
-import { signInWithGooglePopup } from "../../utils/firebase.utils";
+import { useEffect } from "react";
 
-import {createUserDocumentFromAuth} from '../../utils/firebase.utils';
+import { 
+    auth,
+    signInWithGooglePopup, 
+    signInWithGoogleRedirect,
+    createUserDocumentFromAuth  
+} from "../../utils/firebase.utils";
+
+import { getRedirectResult } from "firebase/auth";
+
 
 const SignIn = () =>{
+
+    //--------------On Mount--------------------
+    useEffect(() => {
+        async function getResults(){
+            const response  = await getRedirectResult(auth); //can view auth as authentication memory our app is tracking regardless of where website is going.
+            if(response)
+            {
+                const {userDocRef} = await createUserDocumentFromAuth(response.user);
+            }
+        }
+        getResults();
+    },[]);
+    //--------------On Mount--------------------
 
     const logGoogleUser = async () =>{
         const {user} = await signInWithGooglePopup();
         const {userDocRef} = await createUserDocumentFromAuth(user);
     }
+
     return(
         <div>
             <h1>Sign in page</h1>
             <button onClick={logGoogleUser}>
-                Sign in with google
+                Sign in with Google Popup
+            </button>
+            <button onClick={signInWithGoogleRedirect}>
+                Sign in with Google Redirect
             </button>
         </div>
     )
--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -13,25 +13,27 @@ const firebaseConfig = {
   storageBucket: "zeno-clothing-db.appspot.com",
   messagingSenderId: "70813505057",
   appId: "1:70813505057:web:202c702686b37c6927f560",
-  measurementId: "G-R33D8XQJ10" //For Firebase JS SDK v7.20.0 and later, measurementId is optiona
+  measurementId: "G-R33D8XQJ10" //For Firebase JS SDK v7.20.0 and later, measurementId is optional
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 const analytics = getAnalytics(firebaseApp);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider(); //can be Github, facebook etc
+googleProvider.setCustomParameters({
     prompt: 'select_account'
 });
 
-export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const auth = getAuth(); //Keeps track of the authentication state of the entire application
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
 //Function to create a user document
 export const createUserDocumentFromAuth = async(userAuth) =>{
+
     const {uid,displayName, email} = userAuth; //destructure to get other key-value pairs from the user
 
     const userDocRef = doc(db, 'users', uid); //find the document that matches the parameters
